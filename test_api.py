7d3593f5f161df3ae91fb8e3829b124352d71dd8
Stanley Ndagi <ndagis@gmail.com>
2023-04-19 16:08:12 +0300

    Add test with marker

--- test_api.py
+++ test_api.py
@@ -1,3 +1,4 @@
+import pytest
 import json
 from api import app
 
@@ -10,3 +11,13 @@ def test_get_all_books():
     assert res[1]['author'] == 'Will'
     assert response.status_code == 200
     assert type(res) is list
+
+@pytest.mark.get_request
+def test_get_book_by_id():
+    response = app.test_client().get('/bookapi/books/1')
+    res = json.loads(response.data.decode('utf-8')).get("Book")
+    print(res)
+    assert res['id'] == 1
+    assert res['author'] == 'Harvard'
+    assert res['title'] == 'CS50'
+    assert response.status_code == 200
